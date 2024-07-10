9043af6a0e27e7e9494ff5b08fd2a9f466169964
Shaq-Mg <shaqmcg0@gmail.com>
2024-07-10 12:49:09 +0100

    Initial Commit

---
+++ Journal/JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalApp.swift
+//  Journal
+//
+//  Created by Shaquille McGregor on 10/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
