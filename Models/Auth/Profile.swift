3ef47beea0683d8e8bfd390042fa5b99bf8bd8c5
Shaq-Mg <shaqmcg0@gmail.com>
2024-07-12 13:42:14 +0100

    Adding isPremium property to Profile model

--- Models/Auth/Profile.swift
+++ Models/Auth/Profile.swift
@@ -11,10 +11,12 @@ struct Profile: Codable {
     let name: String
     let email: String
     let photoUrl: String
+    var isPremium: Bool
     
     init(name: String? = nil, email: String? = nil, photoUrl: String? = nil) {
         self.name = name ?? "n/a"
         self.email = email ?? "n/a"
         self.photoUrl = photoUrl ?? "n/a"
+        self.isPremium = false
     }
 }
