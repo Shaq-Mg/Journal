a2d3e991bd68e421f896c2192486f165e0dbe5db
Shaq-Mg <shaqmcg0@gmail.com>
2024-09-02 23:43:27 +0100

    Client feature & viewModel  added

---
+++ Core/Client/ClientView.swift
@@ -0,0 +1,18 @@
+//
+//  ClientView.swift
+//  Journal
+//
+//  Created by Shaquille McGregor on 02/09/2024.
+//
+
+import SwiftUI
+
+struct ClientView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClientView()
+}
