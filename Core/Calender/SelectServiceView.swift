f5d985f7d56ec09bcea2b9c2fa41accf9f0954c0
Shaq-Mg <shaqmcg0@gmail.com>
2024-09-05 23:07:31 +0100

    plus SelectServiceView

---
+++ Core/Calender/SelectServiceView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectServiceView.swift
+//  Journal
+//
+//  Created by Shaquille McGregor on 05/09/2024.
+//
+
+import SwiftUI
+
+struct SelectServiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectServiceView()
+}
