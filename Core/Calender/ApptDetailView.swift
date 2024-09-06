49208273a93bec098e304b49a7ca5264d6d55987
Shaq-Mg <shaqmcg0@gmail.com>
2024-09-06 17:31:22 +0100

    CRUD functions added to CalenderViewModel to manage appointments, ApptDetailView also created 😎

---
+++ Core/Calender/ApptDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ApptDetailView.swift
+//  Journal
+//
+//  Created by Shaquille McGregor on 06/09/2024.
+//
+
+import SwiftUI
+
+struct ApptDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ApptDetailView()
+}
