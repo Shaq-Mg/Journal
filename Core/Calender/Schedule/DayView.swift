c8f171c421d2ca486f8b148cf59ea64ae9e5d017
Shaq-Mg <shaqmcg0@gmail.com>
2024-11-01 17:43:41 +0000

    ReuableInputView created for client name section for BookApptView

---
+++ Core/Calender/Schedule/DayView.swift
@@ -0,0 +1,42 @@
+//
+//  DayView.swift
+//  Journal
+//
+//  Created by Shaquille McGregor on 06/09/2024.
+//
+
+import SwiftUI
+
+struct DayView: View {
+    @EnvironmentObject var vm: CalenderViewModel
+    @Environment(\.dismiss) var dismiss
+    @Binding var showSideMenu: Bool
+    
+    var body: some View {
+        VStack {
+            ReusableHeader(showSideMenu: $showSideMenu, title: vm.selectedDate.dayOfTheWeek())
+            List {
+                Section("Bookings today") {
+                    ForEach(vm.appointments) { appointment in
+                        ZStack {
+                            NavigationLink(destination: ApptDetailView(appt: appointment)) {
+                                EmptyView()
+                            }
+                            .opacity(0)
+                            ApptCellView(appointment: appointment)
+                        }
+                    }
+                }
+            }
+            .onAppear { vm.fetchAppts() }
+        .navigationBarBackButtonHidden(true)
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        DayView(showSideMenu: .constant(false))
+            .environmentObject(CalenderViewModel(database: FirebaseService()))
+    }
+}
---
+++ Core/Calender/Schedule/ScheduleView.swift
@@ -0,0 +1,18 @@
+//
+//  ScheduleView.swift
+//  Journal
+//
+//  Created by Shaquille McGregor on 01/11/2024.
+//
+
+import SwiftUI
+
+struct ScheduleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScheduleView()
+}
